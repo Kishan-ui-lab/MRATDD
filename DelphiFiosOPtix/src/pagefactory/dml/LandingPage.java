8435b2c0a463b2066853e42470dc5fbdd2f53777
Kishan Kumar <kishankumar@Kishans-MacBook-Pro.local>
2021-05-28 21:08:26 -0500

    resolved

--- DelphiFiosOPtix/src/pagefactory/dml/LandingPage.java
+++ DelphiFiosOPtix/src/pagefactory/dml/LandingPage.java
@@ -2,19 +2,20 @@ package pagefactory.dml;
 
 public class LandingPage {
 	
-	int a =1;
+	int a =14;
 	int b = 3;
 	
 	if (b>a) {
 		
 		System.out.println("b is greater than a");
 		
+	} else
+	{
+		
+		System.out.println("a is greater than b");
 	}
 	
 //	String x = "Kishan";
 //	String x = "Kumar";
-	
-	
-	
 
 }
