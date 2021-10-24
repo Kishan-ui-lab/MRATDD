fd17fbfc1e7d4c60c354254df351d35fa05dde36
Kishan <84682244+Kishan-ui-lab@users.noreply.github.com>
2021-10-23 21:33:43 -0400

    Update BaseTestCase.java

--- DelphiFiosOPtix/src/common/BaseTestCase.java
+++ DelphiFiosOPtix/src/common/BaseTestCase.java
@@ -1,59 +1,5 @@
 package common;
 
 public class BaseTestCase {
-	
-	
-	Public webdriver driver;
-	HelperFunctions helper = new HelperFunctions();
-	public Utilities utility;        
-	
-	  Public final string environment = system.getproperty("env","DMLFAKEUAT");
-	  Public final string applpropfilepath = "application.properities";
-	  public String url = null;
-
-
-	system.setproperty("webdriver.chrome.driver","c:\\selenium\\chromedriver.exe");
-	String host = system.getproperty("user.name");
-	Chromeoptions options = new chromeoptions();
-	   
-	driver.manage().window().maximize();
-
-	switch(environment) {
-
-	 Case "DMLFAKEUAT" :
-	    Url = utilities.getDataFromProp(applPropFilePath,"DML_FAKE_UAT");
-	    Sop("url is ..............................................." + url);
-	    break;
-
-	Case "DMLFAKEUATPAM" :
-	    Url = utilities.getDataFromProp(applPropFilePath,"DML_FAKE_UAT_PAM");
-	    Sop("url is ..............................................." + url);
-	    break;
-	}
-	
-	default:
-		PrintUtility.debug("Please specify any one of the Environment(UAT/PROD/PILOT)");
-		break;
-}
-
-
-if(url !=null && environment.equals("DMLFAKEUAT")) {
-	driver.get(url);
-	
-}
-
-else if (url !=null && environment.equals("DMLFAKEUATPAM")) {
-	driver.get(url);
-	
-}
-else {
-	
-
-}
-	Public void closeBrowser() {
-	   
-	  driver.quit();
-
-	}
 
 }
